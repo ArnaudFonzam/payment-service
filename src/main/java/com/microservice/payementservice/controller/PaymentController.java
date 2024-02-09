05f18a66b4bfb87926d8a6739ad9f4f84bb756e4
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-02-09 14:47:42 +0100

    fix error

--- src/main/java/com/microservice/payementservice/controller/PaymentController.java
+++ src/main/java/com/microservice/payementservice/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package com.microservice.payementservice.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
